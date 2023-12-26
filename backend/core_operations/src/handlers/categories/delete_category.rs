aff954aec906c4727e78482f983ad3ff270ed680
sougata-bhattacharjee <sougata.bhattacharjee94@gmail.com>
2023-12-26 14:02:52 +0530

    Added Category APIs

---
+++ backend/core_operations/src/handlers/categories/delete_category.rs
@@ -0,0 +1,49 @@
+use crate::handlers::db_errors::map_db_error_to_status;
+use core_db_entities::entity::categories;
+use proto::proto::core::{CategoriesResponse, CategoryResponse, DeleteCategoryRequest};
+use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter};
+use tonic::{Request, Response, Status};
+
+pub async fn delete_category(
+    db: &DatabaseConnection,
+    request: Request<DeleteCategoryRequest>,
+) -> Result<Response<CategoriesResponse>, Status> {
+    let req = request.into_inner();
+
+    let category = categories::Entity::find_by_id(req.category_id)
+        .one(db)
+        .await;
+
+    match category {
+        Ok(Some(model)) => {
+            match categories::Entity::delete_many()
+                .filter(categories::Column::CategoryId.eq(req.category_id))
+                .exec(db)
+                .await
+            {
+                Ok(delete_result) => {
+                    if delete_result.rows_affected > 0 {
+                        let response = CategoriesResponse {
+                            items: vec![CategoryResponse {
+                                name: model.name,
+                                category_id: model.category_id,
+                            }],
+                        };
+                        Ok(Response::new(response))
+                    } else {
+                        Err(Status::not_found(format!(
+                            "Cart item with ID {} not found.",
+                            req.category_id
+                        )))
+                    }
+                }
+                Err(e) => Err(map_db_error_to_status(e)),
+            }
+        }
+        Ok(None) => Err(Status::not_found(format!(
+            "Cart item with ID {} not found.",
+            req.category_id
+        ))),
+        Err(e) => Err(map_db_error_to_status(e)),
+    }
+}
---
+++ backend/core_operations/src/handlers/categories/mod.rs
@@ -0,0 +1,11 @@
+pub mod create_category;
+pub use create_category::*;
+
+pub mod search_category;
+pub use search_category::*;
+
+pub mod delete_category;
+pub use delete_category::*;
+
+pub mod update_category;
+pub use update_category::*;
