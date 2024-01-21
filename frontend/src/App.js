935cf2b57c529f49f9fba32b0c80e80edbcd5032
Shiftlogic <shiftlogicindia@gmail.com>
2024-01-22 03:45:59 +0530

    Added initial design for Sudattas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//Common Components
+import Navbar from '../src/common/Navbar';
+import Newsletter from '../src/common/Newsletter';
+import Footer from '../src/common/Footer';
+import Copyright from '../src/common/Copyright';
+
+//Pages
+import Layout from './layout/Layout';
+import Home from './pages/Home';
+import Category from './pages/Category';
+import ProductDetail from './pages/ProductDetail';
+import Cart from './pages/Cart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="category" element={<Category />} />
+          <Route path="product" element={<ProductDetail />} />
+          <Route path="cart" element={<Cart />} />
+          {/* <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/FeaturedProduct.js
@@ -0,0 +1,187 @@
+import '../App.css';
+import React, { Component } from 'react';
+
+// Images
+import product1 from '../images/category-image1.jpg';
+import product2 from '../images/category-image2.jpg';
+import product3 from '../images/category-image3.jpg';
+import product4 from '../images/product1.jpg';
+
+class FeatureProduct extends Component {
+
+    render() {
+        return (
+            <div className='body'>
+                
+                <div className="home">
+                    <div className='header'>
+                        <h1>Featured Products</h1>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    </div>
+                    <div className='product-inner'>
+                        <div className='product-single'>
+                            <img src={product1} alt="Product Name" width="100%" />
+                            <ul>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star-o'></li>
+                            </ul>
+                            <p>Product Name</p>
+                            <h4>₹ 5,000</h4>
+                            <button className='btn'>Add To Cart</button>
+                        </div>
+                        <div className='product-single'>
+                            <img src={product2} alt="Product Name" width="100%" />
+                            <ul>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star-o'></li>
+                            </ul>
+                            <p>Product Name</p>
+                            <h4>₹ 5,000</h4>
+                            <button className='btn'>Add To Cart</button>
+                        </div>
+                        <div className='product-single'>
+                            <img src={product3} alt="Product Name" width="100%" />
+                            <ul>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star-o'></li>
+                            </ul>
+                            <p>Product Name</p>
+                            <h4>₹ 5,000</h4>
+                            <button className='btn'>Add To Cart</button>
+                        </div>
+                        <div className='product-single'>
+                            <img src={product4} alt="Product Name" width="100%" />
+                            <ul>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star-o'></li>
+                            </ul>
+                            <p>Product Name</p>
+                            <h4>₹ 5,000</h4>
+                            <button className='btn'>Add To Cart</button>
+                        </div>
+                        <div className='product-single'>
+                            <img src={product1} alt="Product Name" width="100%" />
+                            <ul>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star-o'></li>
+                            </ul>
+                            <p>Product Name</p>
+                            <h4>₹ 5,000</h4>
+                            <button className='btn'>Add To Cart</button>
+                        </div>
+                        <div className='product-single'>
+                            <img src={product2} alt="Product Name" width="100%" />
+                            <ul>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star-o'></li>
+                            </ul>
+                            <p>Product Name</p>
+                            <h4>₹ 5,000</h4>
+                            <button className='btn'>Add To Cart</button>
+                        </div>
+                        <div className='product-single'>
+                            <img src={product3} alt="Product Name" width="100%" />
+                            <ul>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star-o'></li>
+                            </ul>
+                            <p>Product Name</p>
+                            <h4>₹ 5,000</h4>
+                            <button className='btn'>Add To Cart</button>
+                        </div>
+                        <div className='product-single'>
+                            <img src={product4} alt="Product Name" width="100%" />
+                            <ul>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star-o'></li>
+                            </ul>
+                            <p>Product Name</p>
+                            <h4>₹ 5,000</h4>
+                            <button className='btn'>Add To Cart</button>
+                        </div>
+                        <div className='product-single'>
+                            <img src={product1} alt="Product Name" width="100%" />
+                            <ul>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star-o'></li>
+                            </ul>
+                            <p>Product Name</p>
+                            <h4>₹ 5,000</h4>
+                            <button className='btn'>Add To Cart</button>
+                        </div>
+                        <div className='product-single'>
+                            <img src={product2} alt="Product Name" width="100%" />
+                            <ul>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star-o'></li>
+                            </ul>
+                            <p>Product Name</p>
+                            <h4>₹ 5,000</h4>
+                            <button className='btn'>Add To Cart</button>
+                        </div>
+                        <div className='product-single'>
+                            <img src={product3} alt="Product Name" width="100%" />
+                            <ul>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star-o'></li>
+                            </ul>
+                            <p>Product Name</p>
+                            <h4>₹ 5,000</h4>
+                            <button className='btn'>Add To Cart</button>
+                        </div>
+                        <div className='product-single'>
+                            <img src={product4} alt="Product Name" width="100%" />
+                            <ul>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star'></li>
+                                <li className='fa fa-star-o'></li>
+                            </ul>
+                            <p>Product Name</p>
+                            <h4>₹ 5,000</h4>
+                            <button className='btn'>Add To Cart</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
+
+}
+
+export default FeatureProduct;
