da7ac9f5bc140ce69c4966a4c94732b6cd98b561
sougata-bhattacharjee <sougata.bhattacharjee94@gmail.com>
2024-02-01 04:37:28 +0530

    Added productImages api

---
+++ backend/core_operations/src/handlers/product_images/update_product_images.rs
@@ -0,0 +1,37 @@
+use crate::handlers::db_errors::map_db_error_to_status;
+use core_db_entities::entity::product_images;
+use proto::proto::core::{ProductImageResponse, ProductImagesResponse, UpdateProductImageRequest};
+use rust_decimal::{
+    prelude::{FromPrimitive, ToPrimitive},
+    Decimal,
+};
+use sea_orm::{ActiveModelTrait, ActiveValue, DatabaseTransaction};
+use tonic::{Request, Response, Status};
+
+pub async fn update_product_image(
+    txn: &DatabaseTransaction,
+    request: Request<UpdateProductImageRequest>,
+) -> Result<Response<ProductImagesResponse>, Status> {
+    let req = request.into_inner();
+
+    let product_images = product_images::ActiveModel {
+        image_id: ActiveValue::Set(req.image_id),
+        product_id: ActiveValue::Set(req.product_id),
+        image_base64: ActiveValue::Set(req.image_base64),
+        alt_text: ActiveValue::Set(req.alt_text),
+    };
+    match product_images.update(txn).await {
+        Ok(model) => {
+            let response = ProductImagesResponse {
+                items: vec![ProductImageResponse {
+                    image_id: model.image_id,
+                    product_id: model.product_id,
+                    image_base64: model.image_base64,
+                    alt_text: model.alt_text,
+                }],
+            };
+            Ok(Response::new(response))
+        }
+        Err(e) => Err(map_db_error_to_status(e)),
+    }
+}
--- backend/database/run_database.sh
+++ backend/database/run_database.sh
@@ -3,6 +3,7 @@
 echo "Building Docker image with no cache..."
 sudo docker build --no-cache -t sudattas-database-local .
 
+# sudo docker run --name db -e MYSQL_ROOT_PASSWORD=12345678 -p 3306:3306 -d sudattas-database-local
 # container_name="sudattas-database-$(openssl rand -hex 6)"
 
 # echo "Container started: $container_name"
