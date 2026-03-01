2dfa5dd511a1396c375af35629f38e36a873983c
Sougata Bhattacharjee <sougata.bhattacharjee@symbola.io>
2026-03-02 02:03:45 +0530

    Added minimal frontend

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders brand and shop section', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/SUDATTA'S/i)).toBeInTheDocument();
+  expect(screen.getByText(/SHOP/i)).toBeInTheDocument();
 });
